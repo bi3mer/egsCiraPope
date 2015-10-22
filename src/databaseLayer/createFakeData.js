5b8da56dc891fbc538b3f6d96bc9b045da40b4b7
bi3mer <colan@comcast.net>
2015-10-22 12:38:00 -0400

    fake database update

--- src/databaseLayer/createFakeData.js
+++ src/databaseLayer/createFakeData.js
@@ -4,45 +4,45 @@
 	var Database  = require('./databaseDispatcher');
 	
 	setTimeout(function(){
-		Database.addTweet('colan is awesome #CAN 2', '@Coaln', 'CAN');
+		Database.addTweet('#OneLovePhilly EGS is awesome #CAN 2', '@colanRulez', 'CAN');
 		setTimeout(function() {
-			Database.addTweet('colan is awesome #CAN 2', '@Coaln', 'CAN');
-			Database.addTweet('colan is awesome #CAN 2', '@Coaln', 'CAN');
-			Database.addTweet('colan is awesome #CAN 2', '@Coaln', 'CAN');
+			Database.addTweet('#OneLovePhilly EGS is awesome #CAN 2', '@colanRulez', 'CAN');
+			Database.addTweet('#OneLovePhilly EGS is awesome #CAN 2', '@colanRulez', 'CAN');
+			Database.addTweet('#OneLovePhilly EGS is awesome #CAN 2', '@colanRulez', 'CAN');
 		}, 2000);
 
-		Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
+		Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
 		setTimeout(function() {
-			Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
-			Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
-			Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
-			Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
-			Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
-			Database.addTweet('colan is awesome #RUS 2', '@Coaln', 'RUS');
+			Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
+			Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
+			Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
+			Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
+			Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
+			Database.addTweet('#OneLovePhilly EGS is awesome #RUS 2', '@colanRulez', 'RUS');
 		}, 2000);
 
-		Database.addTweet('colan is awesome #ZAF 2', '@Coaln', 'ZAF');
+		Database.addTweet('#OneLovePhilly EGS is awesome #ZAF 2', '@colanRulez', 'ZAF');
 		setTimeout(function() {
-			Database.addTweet('colan is awesome #ZAF 2', '@Coaln', 'ZAF');
-			Database.addTweet('colan is awesome #ZAF 2', '@Coaln', 'ZAF');
-			Database.addTweet('colan is awesome #ZAF 2', '@Coaln', 'ZAF');
-			Database.addTweet('colan is awesome #ZAF 2', '@Coaln', 'ZAF');
+			Database.addTweet('#OneLovePhilly EGS is awesome #ZAF 2', '@colanRulez', 'ZAF');
+			Database.addTweet('#OneLovePhilly EGS is awesome #ZAF 2', '@colanRulez', 'ZAF');
+			Database.addTweet('#OneLovePhilly EGS is awesome #ZAF 2', '@colanRulez', 'ZAF');
+			Database.addTweet('#OneLovePhilly EGS is awesome #ZAF 2', '@colanRulez', 'ZAF');
 		}, 2000);
 
-		Database.addTweet('colan is awesome #BRA 2', '@Coaln', 'BRA');
+		Database.addTweet('#OneLovePhilly EGS is awesome #BRA 2', '@colanRulez', 'BRA');
 
-		Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
+		Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
 		setTimeout(function() {
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
-			Database.addTweet('colan is awesome #USA 2', '@Coaln', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhillyEGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet('#OneLovePhilly EGS is awesome #USA 2', '@colanRulez', 'USA');
+			Database.addTweet(' #OneLovePhillyEGS is awesome #USA 2', '@colanRulez', 'USA');
 		}, 2000);
 
 		setTimeout(function() {
